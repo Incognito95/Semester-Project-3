b9ebbb7a4b48ec4bb82f727bcf85966f41bfea11
incognito95 <danielweisspedersen95@hotmail.com>
2021-12-04 12:18:25 +0100

    updated database

--- Database/semester-project.sql
+++ Database/semester-project.sql
@@ -16,34 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `movie_info`
+-- Table structure for table `movieinfo`
 --
 
-DROP TABLE IF EXISTS `movie_info`;
+DROP TABLE IF EXISTS `movieinfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `movie_info` (
-  `movie_info_id` int NOT NULL AUTO_INCREMENT,
-  `fk_user_name` varchar(45) DEFAULT NULL,
-  `fk_movie_id` varchar(45) DEFAULT NULL,
-  `comment` varchar(45) DEFAULT NULL,
-  `rating` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`movie_info_id`),
-  KEY `fk_movie_info_users1_idx` (`fk_user_name`),
-  KEY `fk_movie_info_movies1_idx` (`fk_movie_id`),
-  CONSTRAINT `fk_movie_info_movies1` FOREIGN KEY (`fk_movie_id`) REFERENCES `mydb`.`movies` (`movie_id`),
-  CONSTRAINT `fk_movie_info_users1` FOREIGN KEY (`fk_user_name`) REFERENCES `users` (`user_name`)
+CREATE TABLE `movieinfo` (
+  `movieInfo_id` bigint NOT NULL AUTO_INCREMENT,
+  `comment` varchar(100) DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  `MOVIES_movie_id` bigint DEFAULT NULL,
+  `USER_user_name` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`movieInfo_id`),
+  KEY `FK_movieinfo_USER_user_name` (`USER_user_name`),
+  KEY `FK_movieinfo_MOVIES_movie_id` (`MOVIES_movie_id`),
+  CONSTRAINT `FK_movieinfo_MOVIES_movie_id` FOREIGN KEY (`MOVIES_movie_id`) REFERENCES `movies` (`movie_id`),
+  CONSTRAINT `FK_movieinfo_USER_user_name` FOREIGN KEY (`USER_user_name`) REFERENCES `users` (`user_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `movie_info`
+-- Dumping data for table `movieinfo`
 --
 
-LOCK TABLES `movie_info` WRITE;
-/*!40000 ALTER TABLE `movie_info` DISABLE KEYS */;
-INSERT INTO `movie_info` VALUES (1,'Admin',NULL,'this is a really good movie',NULL),(2,NULL,NULL,'this is a really bad movie',NULL);
-/*!40000 ALTER TABLE `movie_info` ENABLE KEYS */;
+LOCK TABLES `movieinfo` WRITE;
+/*!40000 ALTER TABLE `movieinfo` DISABLE KEYS */;
+INSERT INTO `movieinfo` VALUES (1,'god film',5,1,'admin'),(2,'hello',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `movieinfo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -54,11 +54,12 @@ DROP TABLE IF EXISTS `movies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `movies` (
-  `movie_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) DEFAULT NULL,
-  `description` text,
+  `movie_id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(2500) DEFAULT NULL,
+  `title` varchar(25) DEFAULT NULL,
+  `images` text,
   PRIMARY KEY (`movie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,32 +68,8 @@ CREATE TABLE `movies` (
 
 LOCK TABLES `movies` WRITE;
 /*!40000 ALTER TABLE `movies` DISABLE KEYS */;
-INSERT INTO `movies` VALUES (4,'far til fire','I denne klassiske familiekomedie bliver en enlig far fyret fra sit job for at være ude af trit med tiden, så hans fire børn giver ham en moderne makeover.'),(5,'toy story','A group of living toys, who assume lifelessness around humans, are preparing to move into a new house with their owner Andy Davis, his sister Molly and their single mother. The toys become uneasy when Andy has his birthday party a week early; to calm them, Sheriff Woody, Andy\'s favorite toy and their leader, sends Sarge and his green army men to spy on the gift opening with a baby monitor. The other toys (which include Mr. Potato Head, Slinky Dog, Rex the tyrannosaur, Hamm the piggy bank, and Bo Peep the porcelain doll) are relieved when Andy receives nothing that could replace them. Andy then receives a last-minute surprise gift – a Buzz Lightyear action figure who believes he is a real space ranger. Buzz impresses the other toys with his various features and becomes Andy\'s new favorite, making Woody jealous.');
+INSERT INTO `movies` VALUES (1,'To protect his family from a powerful drug lord, skilled thief Mehdi and his expert team of robbers are pulled into a violent and deadly turf war.','Ganglands','ganglands.jpg'),(2,'As her father nears the end of his life, filmmaker Kirsten Johnson stages his death in inventive and comical ways to help them both face the inevitable.','Dick Johnson Is Dead','dick_johnson.jpeg'),(3,'Feuds, flirtations and toilet talk go down among the incarcerated women at the Orleans Justice Center in New Orleans on this gritty reality series.','Jailbirds New Orleans','new_orleans.jpg'),(4,'After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth.','Blood & Water','blood_water.jpg');
 /*!40000 ALTER TABLE `movies` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `RENAMEME`
---
-
-DROP TABLE IF EXISTS `RENAMEME`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `RENAMEME` (
-  `ID` bigint NOT NULL AUTO_INCREMENT,
-  `DUMMYSTR1` varchar(255) DEFAULT NULL,
-  `DUMMYSTR2` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `RENAMEME`
---
-
-LOCK TABLES `RENAMEME` WRITE;
-/*!40000 ALTER TABLE `RENAMEME` DISABLE KEYS */;
-/*!40000 ALTER TABLE `RENAMEME` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -166,7 +143,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('admin','$2a$10$aOYTHiI8j2z2wHXNd6sQkun/SOb9iosuBTkAAw8b8kf9mz10EhthG','$2a$10$aOYTHiI8j2z2wHXNd6sQku'),('user','$2a$10$j/8IETiRH2ixLaKiSJAP1urIVkcU3iTJ4xQkGhrGf4x2WBwDBk9Xi','$2a$10$j/8IETiRH2ixLaKiSJAP1u'),('user_admin','$2a$10$T9OzlU2gCPP0Svjh7luuA./tNtX1yrZL10LUnFGZFT3m4twe.zdu2','$2a$10$T9OzlU2gCPP0Svjh7luuA.');
+INSERT INTO `users` VALUES ('admin','$2a$10$IFxe33v3fQS041nsUDZjfOFNuJzDP6RQGdPZ04q7lXnAl96JKK94C','$2a$10$IFxe33v3fQS041nsUDZjfO'),('user','$2a$10$omZpxCJXgZr2CEtRc8kVv..e/186St5Y03zwH6QG6HHuS5L0qXL8q','$2a$10$omZpxCJXgZr2CEtRc8kVv.'),('user_admin','$2a$10$34i/0YBe4YSPUyjZ7../UOuMCU8zH6eGjL98nz9sFnoe.WlXrzD.y','$2a$10$34i/0YBe4YSPUyjZ7../UO');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -179,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-29 11:40:04
+-- Dump completed on 2021-12-04 12:17:28
